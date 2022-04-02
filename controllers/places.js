4e9a71af88aaafaafb795002fed91a96a67baeb8
Nghia Nguyen <aihgn224@gmail.com>
2022-04-01 23:25:19 -0700

    show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -25,6 +25,16 @@ router.get('/new', (req, res) => {
   })
   
   router.get('/:id', (req, res) => {
-    res.render('places/show')
+      let id = Number(req.params.id)
+      if (isNaN(id)) {
+          res.render('error404')
+      }
+      else if (!places[id]) {
+          res.render('error404')
+        }
+      else {
+    res.render('places/show', {place:places[id]})
+    }
+
   })
 module.exports = router
