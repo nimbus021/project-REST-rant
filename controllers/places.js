9a3083207d76502958e943e836d83ddd208cb369
Nghia Nguyen <aihgn224@gmail.com>
2022-04-06 20:55:03 -0700

    added commentary, rants, and raves

--- controllers/places.js
+++ controllers/places.js
@@ -41,12 +41,35 @@ const db = require('../models')
   
   router.get('/:id', (req, res) => {
     db.Place.findById(req.params.id)
+    .populate('comments')
     .then(place =>{
+      console.log(place.comments)
       res.render('places/show', {place})
     })
     .catch('err',err)
     res.render('error404')
   })
+
+  router.post('/:id/comment', (req, res) => {
+    console.log(req.body)
+    db.Place.findById(req.params.id)
+    .then(place => {
+        db.Comment.create(req.body)
+        .then(comment => {
+            place.comments.push(comment.id)
+            place.save()
+            .then(() => {
+                res.redirect(`/places/${req.params.id}`)
+            })
+        })
+        .catch(err => {
+            res.render('error404')
+        })
+    })
+    .catch(err => {
+        res.render('error404')
+    })
+})
   
   router.put('/:id', (req, res) => {
     res.send('PUT /places/:id stub')
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,26 @@ const React = require('react')
 const Def = require('..default')
 
 function show (data) {
+        let comments = (
+          <h3 className="inactive">
+            No comments yet!
+          </h3>
+        )
+        if (data.place.comments.length) {
+          comments = data.place.comments.map(c => {
+            return (
+              <div className="border">
+                <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
+                <h4>{c.content}</h4>
+                <h3>
+                  <strong>- {c.author}</strong>
+                </h3>
+                <h4>Rating: {c.stars}</h4>
+              </div>
+            )
+          })
+        }
+    
     return (
         <Def>
             <main>
@@ -35,6 +55,12 @@ function show (data) {
                 <button type='submit' className='btn btn-danger'>Delete
                 </button>
             </form>
+            <div className="row">
+                     </div>
+            <hr />
+            <h2>Comments</h2>
+            {comments}
+
         </Def>
     )
 }
